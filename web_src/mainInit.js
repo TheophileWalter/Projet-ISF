36acf29a6b0dd4f4b5337bba2aec7f9b03ac4d3c
TheophileWalter <theophilewalter@gmail.com>
2018-10-02 09:38:14 +0200

    Added map

--- web_src/mainInit.js
+++ web_src/mainInit.js
@@ -3,15 +3,24 @@ function mainInit() {
     // Iteration on years
     Object.keys(data).forEach(function(key) {
         // Iteration on values
+        // Create an array containing the geo locations
+        var preparedLocations = [];
         for (var i = 0; i < data[key].length; i++) {
             var code = data[key][i]['Code commune'];
-            var name = data[key][i]['Commune']
+            var name = data[key][i]['Commune'];
             var loc = locationFinder(code, name);
             if (loc === undefined) {
                 console.log('Error on ' + code + ' year ' + key);
                 console.log(data[key][i]);
+            } else {
+                preparedLocations.push([loc['lat'], loc['lon'], data[key][i]['Impôt moyen en €']/1000, '#AA0000']);
             }
         }
+
+        // Creates the map
+        var graph = new ResizableGraph(500, 250, document.body, Map);
+        graph.draw(preparedLocations);
+
     });
     alert('all loaded');
 }
