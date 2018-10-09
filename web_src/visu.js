48e7cb5588cfcc181428b7bd6b661e089338ff81
TheophileWalter <theophilewalter@gmail.com>
2018-10-09 11:13:44 +0200

    Added map

--- web_src/visu.js
+++ web_src/visu.js
@@ -6,9 +6,44 @@ function addVisualization(title, content) {
         "And the lamp-light o’er him streaming throws his shadow on the floor;<br />" +
         "And my soul from out that shadow that lies floating on the floor<br />" +
         "Shall be lifted—nevermore!");
+    addMap("Carte de France", null);
+}
+
+// Add a map to the page
+function addMap(title, geoData) {
+
+    // Create a visualization
+    var v = appendToBody(title, "");
+
+    // Display Map
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaXJlcm8iLCJhIjoiY2ptdzIwc2c0MzN6dzNrb2U0dHp6MzFpeiJ9.IyEBth3DnsPiNmeWGvnhpg';
+    var map = new mapboxgl.Map({
+        container: v.content,
+        style: 'mapbox://styles/mapbox/streets-v10',
+        zoom: 5.4,
+        center: [2.5, 46.5]
+    });
+
+    // Add navigation controls
+    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+
+    // Draw coordinates
+    function setCoords(x, y, text) {
+        var pointCoord = [x, y];
+        marker = new mapboxgl.Marker()
+                .setLngLat(pointCoord)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(text))
+                .addTo(map);		
+    }
+
+    setCoords(2.5, 46.5, "Texte");
+
 }
 
 // Append a visualization to the page
 function appendToBody(title, content) {
-    document.getElementById('page-content').innerHTML += '<div class="visualization"><div class="title">' + escapeHtml(title) + '</div><div class="content">' + content + '</div></div>';
+    var r = Math.random();
+    var id = {"visualization": "visualization-" + r, "title": "title-" + r, "content": "content-" + r};
+    document.getElementById('page-content').innerHTML += '<div class="visualization" id="' + id.visualization + '"><div class="title" id="' + id.title + '">' + escapeHtml(title) + '</div><div class="content" id="' + id.content + '">' + content + '</div></div>';
+    return id;
 }
