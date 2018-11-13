1b97ce9b731931117a3365bd4aaef1d7a564e3a8
TheophileWalter <theophilewalter@gmail.com>
2018-11-13 09:47:51 +0100

    Corrected multiple map problem

--- web_src/visu.js
+++ web_src/visu.js
@@ -1,12 +1,67 @@
+var currentPres = 0;
+
 // Button to add a visualization
 function addVisualization(title, content) {
-    appendToBody("Hello ceci est ma visualisation", "And the Raven, never flitting, still is sitting, still is sitting<br />" +
-        "On the pallid bust of Pallas just above my chamber door;<br />" +
-        "And his eyes have all the seeming of a demon’s that is dreaming,<br />" +
-        "And the lamp-light o’er him streaming throws his shadow on the floor;<br />" +
-        "And my soul from out that shadow that lies floating on the floor<br />" +
-        "Shall be lifted—nevermore!");
-    addMap("Carte de France", fullLocations);
+    switch(currentPres) {
+        case 1:
+            appendToBody("Hello ceci est ma visualisation", "And the Raven, never flitting, still is sitting, still is sitting<br />" +
+            "On the pallid bust of Pallas just above my chamber door;<br />" +
+            "And his eyes have all the seeming of a demon’s that is dreaming,<br />" +
+            "And the lamp-light o’er him streaming throws his shadow on the floor;<br />" +
+            "And my soul from out that shadow that lies floating on the floor<br />" +
+            "Shall be lifted—nevermore!");
+        break;
+        case 0:
+            addMap("Carte de France", fullLocations);
+        break;
+        case 2:
+            addTestChart();
+        break;
+    }
+    currentPres++;
+}
+
+// Add a test chart
+function addTestChart() {
+    var chartId = 'chartjs-' + Math.random();
+    appendToBody('Chart test', '<canvas id="' + chartId + '" width="400" height="400"></canvas>');
+    var ctx = document.getElementById(chartId).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
 }
 
 // Add a map to the page
@@ -46,6 +101,21 @@ function addMap(title, geoData) {
 function appendToBody(title, content) {
     var r = Math.random();
     var id = {"visualization": "visualization-" + r, "title": "title-" + r, "content": "content-" + r};
-    document.getElementById('page-content').innerHTML += '<div class="visualization" id="' + id.visualization + '"><div class="title" id="' + id.title + '">' + escapeHtml(title) + '</div><div class="content" id="' + id.content + '">' + content + '</div></div>';
+
+    var div = document.createElement('div');
+    div.className = 'visualization';
+    div.id = id.visualization;
+    var divTitle = document.createElement('div');
+    divTitle.className = 'title';
+    divTitle.id = id.title;
+    divTitle.innerHTML = escapeHtml(title);
+    div.appendChild(divTitle);
+    var divContent = document.createElement('div');
+    divContent.className = 'content';
+    divContent.id = id.content;
+    divContent.innerHTML = content;
+    div.appendChild(divContent);
+    document.getElementById('page-content').appendChild(div);
+    
     return id;
 }
