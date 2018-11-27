a4f5eb4d693e292c1218785905566503ddce88a8
AlexisMarembert <alexismarembert@gmail.com>
2018-11-27 10:20:51 +0100

    modify color point map

--- web_src/visu.js
+++ web_src/visu.js
@@ -325,10 +325,10 @@ function addMap(title, geoData, typeMap) {
                     source: 'source',
                     filter: ['!has', 'point_count'],
                     paint: {
-                        'circle-color': '#1EF008',
+                        'circle-color': '#2ecc71',
                         'circle-radius': 6,
-                        'circle-stroke-width': 1,
-                        'circle-stroke-color': '#fff'
+                        'circle-stroke-width': 2,
+                        'circle-stroke-color': '#2d3436'
                     }
                 });
 
